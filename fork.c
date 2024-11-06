d790dd72ac771c1079576b5bd862fdab7d5bae91
AlexiaGD12 <alexiagarciadehesa@gmail.com>
2024-11-06 14:40:13 +0000

    feat: add fotk

---
+++ fork.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    int a = 5;
+
+    int pid = fork();
+    if(pid==0){
+        printf("Soy el proceso hijo a = %d\n", a);    
+        execl("./hola", "hola", "juan", "yo", (char *) NULL);
+        printf("ESTO NO DEBERIA IMPRIMIRSE");
+
+        return 0;
+    }
+    
+    printf("Soy el proceso padre a = %d\n", a);
+    
+    return 0;
+}
